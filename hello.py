8e0a924ae61a945a95f26d6d5efd196fc1c8ae61
kjeff91 <kevin.jeffries.jr@gmail.com>
2022-05-05 20:30:36 -0600

    Adding Python script

---
+++ hello.py
@@ -0,0 +1,7 @@
+x=float(input("Enter first number: "))
+y=float(input("Enter second number: "))
+z=float(input("Enter third number: "))
+
+print("The max value is : ", max(x, y, z))
+print("This is Mike's edit!")
+input("Press any key to close")
