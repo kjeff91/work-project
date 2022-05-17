3571cfae0c1514931fe43a884023e9ed138be223
kjeff91 <kevin.jeffries.jr@gmail.com>
2022-05-17 13:00:32 -0600

    added docker compose files

--- Scripts/hello.py
+++ Scripts/hello.py
@@ -3,5 +3,4 @@ y=float(input("Enter second number: "))
 z=float(input("Enter third number: "))
 
 print("The max value is : ", max(x, y, z))
-print("This is Mike's edit!")
 input("Press any key to close")
